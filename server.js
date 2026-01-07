ff1b3bc19ebd5931bb8e0c547f531784bcef2d3d
Amanda Ogletree <amandaogletree19@gmail.com>
2026-01-07 15:23:10 -0500

    Created route handler for get request to root URL and contact url. Added message to log on server start.

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express') //import express
 const app = express() //instance of express
 
 const path = require('path') //import path
+const port = 3000
 
-const port = 3001
+//Middleware, using absolute paths.
+//using the index absolute path results in an error.
+app.get('/', (req, res) => { //Get request handler
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+}) // to Home/Root
 
+app.get('/contact', (req, res) => { //Get request handler
+    res.sendFile(path.join(__dirname, 'public/contact.html'))
+}) //to Contact
+
+
+app.listen(port, () =>{
+    console.log(`Server is running at http://localhost:${port}. 
+        Go check it out! （*゜ー゜*）`)
+})
